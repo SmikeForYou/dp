39620fd8c2a15107306470b3e125eb9af6c955be
Anatolii Savran <a.savran@booongo.com>
2022-11-17 02:44:36 +0200

    Cahce

---
+++ utils.go
@@ -0,0 +1,32 @@
+package dp
+
+type Cache[K comparable, V any] map[K]V
+
+func NewCache[K comparable, V any]() Cache[K, V] {
+	return make(map[K]V)
+}
+
+func (c Cache[K, V]) Push(key K, val V) {
+	c[key] = val
+}
+
+func (c Cache[K, V]) Exists(key K) bool {
+	_, ok := c[key]
+	return ok
+}
+
+func (c Cache[K, V]) Get(key K) (V, bool) {
+	v, ok := c[key]
+	return v, ok
+}
+func (c Cache[K, V]) GetWithCallback(key K, onFail func(key K) (V, bool)) (V, bool) {
+	v, ok := c[key]
+	if ok {
+		return v, ok
+	}
+	return onFail(key)
+}
+
+func (c Cache[K, V]) Remove(key K) {
+	delete(c, key)
+}
